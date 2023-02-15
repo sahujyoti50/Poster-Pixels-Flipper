e12d6eda51f0221ed81177d204af6a3ee4294f64
sahujyoti50 <sahujyoti50@gmail.com>
2023-02-15 13:47:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import logo from './logo.svg';
+import './App.css';
+import Temp from './temp';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f1.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f2.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f3.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f4.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f5.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f6.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f7.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f8.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f9.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f10.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f11.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f12.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f13.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f14.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f15.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f16.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+
+      <div class="two-sided-effect">
+        <div class="flipper">
+          <div class="front">
+            <img src="f17.jpg" alt="front" />
+          </div>
+          <div class="back">
+            <img src="f18.jpg" alt="back" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/temp.js
@@ -0,0 +1,15 @@
+export default function Temp(){
+return(
+      <div class="grid">
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>  
+      </div>
+)
+}
